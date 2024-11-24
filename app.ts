bd15533bb913e115f993a4ef39f8ca520aa2ff37
Alish142 <a.ranabhat.12@student.scu.edu.au>
2024-11-24 17:52:42 +1000

    Update app.ts

--- app.ts
+++ app.ts
@@ -11,7 +11,46 @@ interface Client {
   isVIP: boolean;
 }
 
-let clients: Client[] = [];
+let clients: Client[] = [
+  {
+    clientID: '1',
+      name: 'Ram Bahadur',
+      dob: '1990-05-15',
+      gender: 'Male',
+      fitnessProgram: 'muscle gain',
+      contactInfo: 'john.doe@email.com',
+      joinedDate: '2023-01-01',
+      endingDate: '2023-12-31',
+      specialHealthNotes: 'No allergies',
+      isVIP: true
+    },
+    {
+      clientID: '2',
+      name: 'KP BAA',
+      dob: '1985-08-22',
+      gender: 'Female',
+      fitnessProgram: 'fat loss',
+      contactInfo: 'jane.smith@email.com',
+      joinedDate: '2023-02-15',
+      endingDate: '2023-11-30',
+      specialHealthNotes: 'Mild asthma',
+      isVIP: false
+    },
+    {
+      clientID: '3',
+      name: 'William Jordan',
+      dob: '1978-11-30',
+      gender: 'Male',
+      fitnessProgram: 'overall fitness',
+      contactInfo: 'bob.johnson@email.com',
+      joinedDate: '2023-03-01',
+      endingDate: '2024-02-29',
+      specialHealthNotes: 'Previous knee injury',
+      isVIP: true
+    }
+];
+
+
 
 // Add or Update Client
 function addOrUpdateClient(event: Event) {
